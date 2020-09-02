3733c43e618cf81f909e5294ca41091efaf302b2
gokulavarathan <61727747+gokulavarathan@users.noreply.github.com>
2020-09-02 21:00:14 +0530

    add initial cv website file

---
+++ java.js
@@ -0,0 +1,11 @@
+function openSideMenu() {
+    document.getElementById("menu").style.width = "100px"
+    document.getElementById("content").style.marginLeft = "250px"
+
+}
+
+function closeSideMenu() {
+    document.getElementById("menu").style.width = "0"
+    document.getElementById("content").style.marginLeft = "0"
+
+}
